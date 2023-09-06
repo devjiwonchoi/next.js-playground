91b3fd74f6d872988a7877971301ee09a43ec312
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-06 16:25:46 +0900

    get ip test

---
+++ app-router/src/app/api/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const runtime = 'edge'
+
+export async function GET(req: NextRequest) {
+  const ip = (req.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
+  return NextResponse.json({ ip })
+}
