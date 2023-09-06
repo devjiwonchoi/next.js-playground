ad8d6f39396ddf283a4db2426b0a74c465dc5347
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-06 16:31:59 +0900

    remove edge from route.ts

--- app-router/src/app/api/route.ts
+++ app-router/src/app/api/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export const runtime = 'edge'
-
 export async function GET(req: NextRequest) {
   const ip = (req.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
   return NextResponse.json({ ip })
